ade71bb66194031b5f44d38fdb42b953ff3d6d47
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-05 15:44:21 -0300

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,14 @@
 CatarsePagarme::Engine.routes.draw do
+  resources :pagarme, only: [], path: "payment/pagarme" do
+
+    collection do
+      post :ipn
+    end
+
+    member do
+      get  :review
+      post :pay
+    end
+
+  end
 end
