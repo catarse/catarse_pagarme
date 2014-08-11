b2cd99decec410e6fbc920d8e72c0504e9db0e93
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-11 16:07:56 -0300

    added slip_tax and credit_card_tax into configuration for get the fee

--- lib/catarse_pagarme/configuration.rb
+++ lib/catarse_pagarme/configuration.rb
@@ -1,9 +1,11 @@
 module CatarsePagarme
   class Configuration
-    attr_accessor :api_key
+    attr_accessor :api_key, :slip_tax, :credit_card_tax
 
     def inititalizer
       @api_key = ''
+      @slip_tax = 0
+      @credit_card_tax = 0
     end
   end
 end
