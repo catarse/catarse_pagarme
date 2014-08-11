f1caff296dbff2893234efda4aa596aff67b8324
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-11 16:55:27 -0300

    added IPN method to receive pagarme notifications

--- app/controllers/catarse_pagarme/pagarme_controller.rb
+++ app/controllers/catarse_pagarme/pagarme_controller.rb
@@ -4,9 +4,6 @@ module CatarsePagarme
     skip_before_filter :force_http
     layout :false
 
-    def ipn
-    end
-
     def pay_credit_card
       transaction = charge_transaction {}
     end
@@ -22,9 +19,37 @@ module CatarsePagarme
     rescue PagarMe::PagarMeError => e
       render json: { boleto_url: nil, payment_status: 'failed', message: e.message }
     end
+
+    def ipn
+      validate_fingerprint do
+        _contribution = PaymentEngines.find_payment(payment_id: params['id'])
+
+        if _contribution
+          _contribution.payment_notificatons.create(extra_data: params)
+
+          case params[:current_status]
+          when 'paid' && !_contribution.confirmed? then
+            _contribution.confirm
+          when 'refunded' && !_contribution.refunded? then
+            _contribution.refund
+          when 'refused' then
+            _contribution.cancel
+          end
+
+          return render nothing: { status: 200 }
+        end
+
+        return render nothing: { status: 404 }
+      end
     end
 
     protected
+
+    def validate_fingerprint
+      if PagarMe::validate_fingerprint(params[:id], params[:fingerprint])
+        yield
+      end
+    end
 
     def charge_transaction(transaction_attributes = {})
       transaction = PagarMe::Transaction.new(transaction_attributes)
@@ -34,7 +59,7 @@ module CatarsePagarme
                                 when 'boleto' then
                                   {
                                     payment_choice: PaymentType::SLIP,
-                                    payment_service_fee: get_fee(PaymentType::SLIP) 
+                                    payment_service_fee: get_fee(PaymentType::SLIP),
                                   }
                                 when 'credit_card' then
                                   {
@@ -45,6 +70,7 @@ module CatarsePagarme
                                   {}
                                 end
 
+      contribution_attributes.merge!({ payment_id: transaction.id })
       contribution.update_attributes(contribution_attributes)
 
       return transaction
