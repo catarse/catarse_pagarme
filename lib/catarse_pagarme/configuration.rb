88f7343e0929b23aa40f1ba33a6521be2f3b9689
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-13 18:42:18 -0300

    added more configurations options about tax

--- lib/catarse_pagarme/configuration.rb
+++ lib/catarse_pagarme/configuration.rb
@@ -1,11 +1,12 @@
 module CatarsePagarme
   class Configuration
-    attr_accessor :api_key, :slip_tax, :credit_card_tax
+    attr_accessor :api_key, :slip_tax, :credit_card_tax, :interest_rate
 
     def inititalizer
-      @api_key = ''
-      @slip_tax = 0
-      @credit_card_tax = 0
+      self.api_key = ''
+      self.slip_tax = 0
+      self.credit_card_tax = 0
+      self.interest_rate = 0
     end
   end
 end
