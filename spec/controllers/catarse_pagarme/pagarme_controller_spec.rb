3eccd4d1950cb5ce2f8ddef1bcbcd5b7d30f8579
Antônio Roberto Silva <forevertonny@gmail.com>
2014-08-22 15:54:44 -0300

    specs for credit card payment controller

--- spec/controllers/catarse_pagarme/pagarme_controller_spec.rb
+++ spec/controllers/catarse_pagarme/pagarme_controller_spec.rb
@@ -6,12 +6,64 @@ describe CatarsePagarme::PagarmeController do
     controller.stub(:current_user).and_return(user)
   end
 
-  describe 'pay with slip' do
-    let(:project) { create(:project, goal: 10_000, state: 'online') }
-    let(:contribution) { create(:contribution, value: 10, project: project) }
-    let(:user) { nil }
+  let(:project) { create(:project, goal: 10_000, state: 'online') }
+  let(:contribution) { create(:contribution, value: 10, project: project) }
 
+  describe 'pay with credit card' do
     context  'without an user' do
+      let(:user) { nil }
+
+      it 'should raise a error' do
+        expect {
+          post :pay_credit_card, { locale: :pt, project_id: project.id, contribution_id: contribution.id, use_route: 'catarse_pagarme' }
+        }.to raise_error('invalid user')
+      end
+    end
+
+    context 'with an user' do
+      let(:user) { contribution.user }
+      context "with valid card data" do
+        before do
+          post :pay_credit_card, {
+            locale: :pt, project_id: project.id, contribution_id: contribution.id, use_route: 'catarse_pagarme',
+            payment_card_date: '10/17', payment_card_number: '4901720080344448', payment_card_name: 'Foo bar',
+            payment_card_source: '143', payment_card_installments: '1' }
+
+        end
+
+        it 'payment_status should be filled and not failed' do
+          expect(ActiveSupport::JSON.decode(response.body)['payment_status']).not_to be_nil
+        end
+
+        it 'and payment_status is not failed' do
+          expect(ActiveSupport::JSON.decode(response.body)['payment_status']).not_to eq('failed')
+        end
+      end
+
+      context 'with invalid card data' do
+        before do
+          post :pay_credit_card, {
+            locale: :pt, project_id: project.id, contribution_id: contribution.id, use_route: 'catarse_pagarme',
+            payment_card_date: '', payment_card_name: '',
+            payment_card_source: '143', payment_card_installments: '1' }
+        end
+
+        it 'payment_status should be failed' do
+          expect(ActiveSupport::JSON.decode(response.body)['payment_status']).to eq('failed')
+        end
+
+        it 'message should be filled' do
+          expect(ActiveSupport::JSON.decode(response.body)['message']).not_to be_nil
+        end
+      end
+    end
+  end
+
+
+  describe 'pay with slip' do
+    context  'without an user' do
+      let(:user) { nil }
+
       it 'should raise a error' do
         expect {
           post :pay_slip, { locale: :pt, project_id: project.id, contribution_id: contribution.id, use_route: 'catarse_pagarme' }
@@ -21,10 +73,6 @@ describe CatarsePagarme::PagarmeController do
 
     context 'with an user' do
       let(:user) { contribution.user }
-
-      before do
-        controller.stub(:current_user).and_return(user)
-      end
 
       context 'with valid bank account data' do
         before do
