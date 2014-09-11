f91a62d332555f112466e6b73d3266c44b3dcecf
Antônio Roberto <forevertonny@gmail.com>
2014-09-11 09:59:46 -0300

    create subscription controller to handle with subscription payments

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ CatarsePagarme::Engine.routes.draw do
       post :ipn, to: 'notifications#create'
       post :pay_credit_card, to: 'credit_cards#create'
       post :pay_slip
-      post :pay_with_subscription
+      post :pay_with_subscription, to: 'subscriptions#create'
+      put :pay_with_subscription, to: 'subscriptions#update'
     end
 
   end
