f1e9abc8c2b5d1cb0332581c3dc9c65b567ea672
Antônio Roberto <forevertonny@gmail.com>
2014-09-30 19:08:14 -0300

    adjust notifications

--- app/controllers/catarse_pagarme/notifications_controller.rb
+++ app/controllers/catarse_pagarme/notifications_controller.rb
@@ -4,15 +4,13 @@ module CatarsePagarme
     skip_before_filter :force_http
 
     def create
-      if PagarMe::validate_fingerprint(contribution.try(:payment_id), params[:fingerprint])
-        if contribution
-          contribution.payment_notifications.create(extra_data: params.to_json)
-          delegator.change_status_by_transaction(params[:current_status])
+      if contribution
+        contribution.payment_notifications.create(extra_data: params.to_json)
 
+        if PagarMe::validate_fingerprint(contribution.try(:payment_id), params[:fingerprint])
+          delegator.change_status_by_transaction(params[:current_status])
           return render nothing: true, status: 200
         end
-
-        return render nothing: true, status: 404
       end
 
       render nothing: true, status: 404
