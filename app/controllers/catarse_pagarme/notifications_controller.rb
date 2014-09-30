4d16e9345d3d4bb2121a16a9e016ab9eec87eefc
Antônio Roberto <forevertonny@gmail.com>
2014-09-30 16:24:35 -0300

    logger into fingerprint checks

--- app/controllers/catarse_pagarme/notifications_controller.rb
+++ app/controllers/catarse_pagarme/notifications_controller.rb
@@ -4,7 +4,11 @@ module CatarsePagarme
     skip_before_filter :force_http
 
     def create
+      Rails.logger.info "Start IPN"
+      Rails.logger.info contribution.try(:payment_id)
+      Rails.logger.info params[:fingerprint]
       if PagarMe::validate_fingerprint(contribution.try(:payment_id), params[:fingerprint])
+        Rails.logger.info "GET FINGERPRINT"
         if contribution
           contribution.payment_notifications.create(extra_data: params.to_json)
           delegator.change_status_by_transaction(params[:current_status])
@@ -14,6 +18,8 @@ module CatarsePagarme
 
         return render nothing: true, status: 404
       end
+
+      Rails.logger.info "CANT CHECK FINGERPRINT :("
 
       render nothing: true, status: 404
     end
