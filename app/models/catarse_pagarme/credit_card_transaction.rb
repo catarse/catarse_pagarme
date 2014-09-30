bce3a6bfc56a1123ab13c8a6ea81033154efca48
Antônio Roberto <forevertonny@gmail.com>
2014-09-30 16:20:35 -0300

    when transaction is refused raise a pagarme error

--- app/models/catarse_pagarme/credit_card_transaction.rb
+++ app/models/catarse_pagarme/credit_card_transaction.rb
@@ -4,6 +4,10 @@ module CatarsePagarme
       self.transaction = PagarMe::Transaction.new(self.attributes)
       self.transaction.charge
 
+      if self.transaction.status == 'refused'
+        raise ::PagarMe::PagarMeError.new(I18n.t('projects.contributions.edit.transaction_error'))
+      end
+
       change_contribution_state
       self.transaction
     end
