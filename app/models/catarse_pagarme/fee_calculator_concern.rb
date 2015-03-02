1b2a72eafa419d38931c0b58e7359e57e803de1d
Diogo Biazus <diogob@gmail.com>
2015-03-02 13:58:39 -0500

    Breaks get_fee method in several smaller methods

--- app/models/catarse_pagarme/fee_calculator_concern.rb
+++ app/models/catarse_pagarme/fee_calculator_concern.rb
@@ -6,24 +6,47 @@ module CatarsePagarme::FeeCalculatorConcern
     def get_fee
       return nil if self.contribution.payment_choice.blank? # We always depend on the payment_choice
       if self.contribution.payment_choice == ::CatarsePagarme::PaymentType::SLIP
-        CatarsePagarme.configuration.slip_tax.to_f
+        get_slip_fee
       else
-        return nil if self.contribution.acquirer_name.blank? # Here we depend on the acquirer name
-        if self.contribution.acquirer_name == 'stone'
-          self.contribution.installments > 1 ? tax_calc_for_installment(stone_tax) : tax_calc(stone_tax)
-        else
-          return nil if self.contribution.card_brand.blank? # Here we depend on the card_brand
-          if self.contribution.card_brand == 'amex'
-            self.contribution.installments > 1 ? tax_calc_for_installment(cielo_installment_amex_tax) : tax_calc(cielo_installment_not_amex_tax)
-          else
-            current_tax = self.contribution.card_brand == 'diners' ? installment_diners_tax : installment_not_diners_tax
-            self.contribution.installments > 1 ? tax_calc_for_installment(current_tax) : tax_calc(cielo_tax)
-          end
-        end
+        get_card_fee
       end
     end
 
     protected
+    def get_slip_fee
+      CatarsePagarme.configuration.slip_tax.to_f
+    end
+
+    def get_card_fee
+      return nil if self.contribution.acquirer_name.blank? # Here we depend on the acquirer name
+      if self.contribution.acquirer_name == 'stone'
+        get_stone_fee
+      else
+        get_cielo_fee
+      end
+    end
+
+    def get_stone_fee
+      self.contribution.installments > 1 ? tax_calc_for_installment(stone_tax) : tax_calc(stone_tax)
+    end
+
+    def get_cielo_fee
+      return nil if self.contribution.card_brand.blank? # Here we depend on the card_brand
+      if self.contribution.card_brand == 'amex'
+        get_cielo_fee_for_amex
+      else
+        get_cielo_fee_for_non_amex
+      end
+    end
+
+    def get_cielo_fee_for_amex
+      self.contribution.installments > 1 ? tax_calc_for_installment(cielo_installment_amex_tax) : tax_calc(cielo_installment_not_amex_tax)
+    end
+
+    def get_cielo_fee_for_non_amex
+      current_tax = self.contribution.card_brand == 'diners' ? installment_diners_tax : installment_not_diners_tax
+      self.contribution.installments > 1 ? tax_calc_for_installment(current_tax) : tax_calc(cielo_tax)
+    end
 
     def tax_calc acquirer_tax
       ((self.contribution.value * pagarme_tax) + cents_fee).round(2) + (self.contribution.value * acquirer_tax).round(2)
