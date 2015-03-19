4430c97ddfdb5ada0f6dce43c1aa9de8ab86bd4c
Antônio Roberto <forevertonny@gmail.com>
2015-03-19 14:19:21 -0300

    use change_contribution_state after charge

--- app/models/catarse_pagarme/credit_card_transaction.rb
+++ app/models/catarse_pagarme/credit_card_transaction.rb
@@ -7,26 +7,24 @@ module CatarsePagarme
       self.transaction = PagarMe::Transaction.new(self.attributes)
       self.transaction.charge
 
+      change_contribution_state
+
       if self.transaction.status == 'refused'
         raise ::PagarMe::PagarMeError.new(I18n.t('projects.contributions.edit.transaction_error'))
       end
 
       save_user_credit_card if save_card
-
-      change_contribution_state
       self.transaction
     end
 
     def save_user_credit_card
-      card = self.transaction.try(:card)
+      card = self.transaction.card
 
-      if card
-        credit_card = self.user.credit_cards.find_or_initialize_by(card_key: card.id)
-        credit_card.last_digits = card.last_digits
-        credit_card.card_brand = card.brand
+      credit_card = self.user.credit_cards.find_or_initialize_by(card_key: card.id)
+      credit_card.last_digits = card.last_digits
+      credit_card.card_brand = card.brand
 
-        credit_card.save
-      end
+      credit_card.save
     end
 
   end
