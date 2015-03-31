eea5e2153fef567b15ada6102aca87d1753a4d95
Diogo Biazus <diogob@gmail.com>
2015-03-31 14:01:37 -0400

    Fixes slip transaction

--- spec/models/catarse_pagarme/slip_transaction_spec.rb
+++ spec/models/catarse_pagarme/slip_transaction_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe CatarsePagarme::SlipTransaction do
-  let(:contribution) { create(:contribution, value: 100) }
-  let(:pagarme_transaction) {
-    double({
+  let(:payment) { create(:payment, value: 100) }
+  let(:pagarme_transaction_attributes) {
+    {
       id: 'abcd',
       charge: true,
       status: 'paid',
@@ -12,8 +12,13 @@ describe CatarsePagarme::SlipTransaction do
       acquirer_name: 'pagarme',
       tid: '123123',
       card_brand: nil
-    })
+    }
   }
+
+  let(:pagarme_transaction) {
+    double(pagarme_transaction_attributes)
+  }
+
   let(:valid_attributes) do
     {
       slip_payment: {
@@ -44,22 +49,23 @@ describe CatarsePagarme::SlipTransaction do
     }
   end
 
-  let(:slip_transaction) { CatarsePagarme::SlipTransaction.new(valid_attributes, contribution) }
+  let(:slip_transaction) { CatarsePagarme::SlipTransaction.new(valid_attributes, payment) }
 
   before do
     PagarMe::Transaction.stub(:new).and_return(pagarme_transaction)
+    pagarme_transaction.stub(:to_json).and_return(pagarme_transaction_attributes.to_json)
     CatarsePagarme::PaymentDelegator.any_instance.stub(:change_status_by_transaction).and_return(true)
   end
 
   context "#user" do
     subject { slip_transaction.user }
-    it { expect(subject).to eq(contribution.user) }
+    it { expect(subject).to eq(payment.user) }
   end
 
   context "#charge!" do
     context "with invalid attributes" do
       let(:slip_transaction) {
-        CatarsePagarme::SlipTransaction.new(invalid_attributes, contribution)
+        CatarsePagarme::SlipTransaction.new(invalid_attributes, payment)
       }
 
       it "should raises an error" do
@@ -73,44 +79,44 @@ describe CatarsePagarme::SlipTransaction do
       before do
         slip_transaction.should_receive(:update_user_bank_account).and_call_original
         slip_transaction.user.should_receive(:update_attributes).and_return(true)
-        contribution.should_receive(:update_attributes).at_least(1).and_call_original
+        payment.should_receive(:update_attributes).at_least(1).and_call_original
         PagarMe::Transaction.should_receive(:find_by_id).with(pagarme_transaction.id).and_return(pagarme_transaction)
         CatarsePagarme::PaymentDelegator.any_instance.should_receive(:change_status_by_transaction).with('paid')
 
         slip_transaction.charge!
-        contribution.reload
+        payment.reload
       end
 
-      it "should update contribution payment_id" do
-        expect(contribution.payment_id).to eq('abcd')
+      it "should update payment payment_id" do
+        expect(payment.gateway_id).to eq('abcd')
       end
 
-      it "should update contribution payment_service_fee" do
-        expect(contribution.payment_service_fee).to eq(0.0)
+      it "should update payment payment_service_fee" do
+        expect(payment.gateway_fee).to eq(0.0)
       end
 
-      it "should update contribution payment_method" do
-        expect(contribution.payment_method).to eq('Pagarme')
+      it "should update payment payment_method" do
+        expect(payment.gateway).to eq('Pagarme')
       end
 
-      it "should update contribution slip_url" do
-        expect(contribution.slip_url).to eq('boleto url')
+      it "should update payment slip_url" do
+        expect(payment.gateway_data["boleto_url"]).to eq('boleto url')
       end
 
-      it "should update contribution payment_choice" do
-        expect(contribution.payment_choice).to eq(CatarsePagarme::PaymentType::SLIP)
+      it "should update payment payment_choice" do
+        expect(payment.payment_method).to eq(CatarsePagarme::PaymentType::SLIP)
       end
 
-      it "should update contribution acquirer_name" do
-        expect(contribution.acquirer_name).to eq('pagarme')
+      it "should update payment acquirer_name" do
+        expect(payment.gateway_data["acquirer_name"]).to eq('pagarme')
       end
 
-      it "should update contribution acquirer_tid" do
-        expect(contribution.acquirer_tid).to eq('123123')
+      it "should update payment acquirer_tid" do
+        expect(payment.gateway_data["acquirer_tid"]).to eq('123123')
       end
 
-      it "should update contribution installment_value" do
-        expect(contribution.installment_value).to_not be_nil
+      it "should update payment installment_value" do
+        expect(payment.installment_value).to_not be_nil
       end
     end
   end
