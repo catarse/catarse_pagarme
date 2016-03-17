5b1313509050a38b928911b8283b304e44f70c8f
Antônio Roberto Silva <forevertonny@gmail.com>
2016-03-17 15:51:11 -0300

    send all customer info to pagarme

--- app/controllers/catarse_pagarme/credit_cards_controller.rb
+++ app/controllers/catarse_pagarme/credit_cards_controller.rb
@@ -13,6 +13,9 @@ module CatarsePagarme
     protected
 
     def credit_card_attributes
+      contribution = payment.contribution
+      phone_matches = contribution.address_phone_number.gsub(/[\s,-]/, '').match(/\((.*)\)(\d+)/)
+
       hash = {
         payment_method: 'credit_card',
         amount: delegator.value_with_installment_tax(get_installment),
@@ -24,8 +27,20 @@ module CatarsePagarme
         soft_descriptor: payment.project.permalink.gsub(/[\W\_]/, ' ')[0, MAX_SOFT_DESCRIPTOR_LENGTH],
         installments: get_installment,
         customer: {
-          email: payment.user.email,
-          name: payment.user.name
+          email: contribution.payer_email,
+          name: contribution.payer_name,
+          document_number: contribution.payer_document.gsub(/[-.\/_\s]/,''),
+          address: {
+            street: contribution.address_street,
+            neighborhood: contribution.address_neighbourhood,
+            zipcode: contribution.address_zip_code.gsub(/[-.]/, ''),
+            street_number: contribution.address_number,
+            complementary: contribution.address_complement
+          },
+          phone: {
+            ddd: phone_matches[1],
+            number: phone_matches[2]
+          }
         },
         metadata: metadata_attributes
       }
