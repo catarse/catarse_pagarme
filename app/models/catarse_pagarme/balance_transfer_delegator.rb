ca18b5ac33cb76f845dd256afb6b3041ba67af8c
Antônio Roberto Silva <forevertonny@gmail.com>
2016-03-31 12:53:14 -0300

    do all transfer process inside transaction block

--- app/models/catarse_pagarme/balance_transfer_delegator.rb
+++ app/models/catarse_pagarme/balance_transfer_delegator.rb
@@ -8,21 +8,24 @@ module CatarsePagarme
     end
 
     def transfer_funds
-      raise "unable to create transfer, need to be authorized" if !balance_transfer.authorized?
+      ActiveRecord::Base.transaction do
+        raise "unable to create transfer, need to be authorized" if !balance_transfer.authorized?
 
-      bank_account = PagarMe::BankAccount.new(bank_account_attributes.delete(:bank_account))
-      bank_account.create
-      raise "unable to create an bank account" unless bank_account.id.present?
+        bank_account = PagarMe::BankAccount.new(bank_account_attributes.delete(:bank_account))
+        bank_account.create
+        raise "unable to create an bank account" unless bank_account.id.present?
 
-      transfer = PagarMe::Transfer.new({
-        bank_account_id: bank_account.id,
-        amount: value_for_transaction
-      })
-      transfer.create
-      raise "unable to create a transfer" unless transfer.id.present?
+        transfer = PagarMe::Transfer.new({
+          bank_account_id: bank_account.id,
+          amount: value_for_transaction
+        })
+        transfer.create
+        raise "unable to create a transfer" unless transfer.id.present?
 
-      balance_transfer.update_attribute(:transfer_id, transfer.id)
-      balance_transfer.transition_to(:processing, transfer_data: transfer.to_hash)
+        balance_transfer.update_attribute(:transfer_id, transfer.id)
+        balance_transfer.transition_to(:processing, transfer_data: transfer.to_hash)
+        balance_transfer
+      end
     end
 
     def bank_account_attributes
@@ -30,7 +33,7 @@ module CatarsePagarme
 
       {
         bank_account: {
-          bank_code: (account.bank.bank_code || account.bank.name),
+          bank_code: (account.bank.code || account.bank.name),
           agencia: account.agency,
           agencia_dv: account.agency_digit,
           conta: account.account,
