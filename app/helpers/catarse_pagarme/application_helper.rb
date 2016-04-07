c3d4e138d64df7d218b6374ab4630e10ab6e82db
Antônio Roberto Silva <forevertonny@gmail.com>
2016-04-07 02:48:42 -0300

    use total_installments from projects

--- app/helpers/catarse_pagarme/application_helper.rb
+++ app/helpers/catarse_pagarme/application_helper.rb
@@ -3,10 +3,11 @@ module CatarsePagarme
 
     def installments_for_select(payment)
       installments = payment.pagarme_delegator.get_installments['installments']
+      project = payment.project
 
       collection = installments.map do |installment|
         installment_number = installment[0].to_i
-        if installment_number <= CatarsePagarme.configuration.max_installments.to_i
+        if installment_number <= (project.try(:total_installments) || CatarsePagarme.configuration.max_installments.to_i)
           amount = installment[1]['installment_amount'] / 100.0
 
           optional_text = nil
