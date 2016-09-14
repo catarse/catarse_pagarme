91cc0584b96b350b1e101079886532d495a8d8d3
Vinicius Andrade <vini.andrade.dev@gmail.com>
2016-09-14 00:43:38 -0300

    Implements new credit card routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ CatarsePagarme::Engine.routes.draw do
       get  :review
       get  :slip_data, to: 'slip#slip_data'
       get  :second_slip, to: 'slip#update'
+      get  :credit_cards, to: 'credit_cards#get_saved_cards'
+      get  :get_installment, to: 'credit_cards#get_installment_json'
       post :pay_credit_card, to: 'credit_cards#create'
       post :pay_slip, to: 'slip#create'
     end
