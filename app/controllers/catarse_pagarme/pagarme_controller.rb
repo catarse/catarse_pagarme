49202f56e0883ee427b33e5b44de1a36181c6726
Antônio Roberto <forevertonny@gmail.com>
2017-01-11 12:36:31 -0200

    Remove unused assets

--- app/controllers/catarse_pagarme/pagarme_controller.rb
+++ app/controllers/catarse_pagarme/pagarme_controller.rb
@@ -1,10 +1,4 @@
 module CatarsePagarme
   class PagarmeController < CatarsePagarme::ApplicationController
-
-    def review
-      contribution
-      current_user.build_bank_account unless current_user.bank_account
-    end
-
   end
 end
