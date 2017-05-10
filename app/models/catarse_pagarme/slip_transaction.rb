e9cbc99c8197e72b684237c048a1788d0927b65d
Antônio Roberto <forevertonny@gmail.com>
2017-05-09 21:31:32 -0300

    handle with duplicated validation for slip payments

--- app/models/catarse_pagarme/slip_transaction.rb
+++ app/models/catarse_pagarme/slip_transaction.rb
@@ -1,6 +1,14 @@
 module CatarsePagarme
   class SlipTransaction < TransactionBase
     def charge!
+      unless payment.update_attributes({
+        gateway: 'Pagarme',
+        payment_method: payment_method})
+
+        raise ::PagarMe::PagarMeError.new(
+          payment.errors.messages.values.flatten.to_sentence)
+      end
+
       self.transaction = PagarMe::Transaction.new(self.attributes)
       self.transaction.charge
 
