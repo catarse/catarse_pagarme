1ff56381cb8a2448d43a2f567755621fdc99982a
stephannv <stephann@protonmail.com>
2020-03-31 02:27:25 -0300

    Fix some production errors

--- app/models/catarse_pagarme/slip_transaction.rb
+++ app/models/catarse_pagarme/slip_transaction.rb
@@ -9,7 +9,9 @@ module CatarsePagarme
           payment.errors.messages.values.flatten.to_sentence)
       end
 
-      self.transaction = PagarMe::Transaction.new(self.attributes.merge(payment_method: 'boleto'))
+      self.transaction = PagarMe::Transaction.new(
+        self.attributes.merge(payment_method: 'boleto', async: false)
+      )
       self.transaction.charge
 
       change_payment_state
